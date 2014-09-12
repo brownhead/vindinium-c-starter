12ce3854dfd964950a213cb23e205667fe8b548d
John Sullivan <josullivan@mozilla.com>
2014-09-12 02:41:04 -0700

    Getting there.

--- src/main.c
+++ src/main.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdio.h>
 #include <curl/curl.h>
 
-typedef CURL VindiniumSession;
-
-enum VindiniumStatus {
+typedef enum {
   VINDINIUM_STATUS_OK = 0,
   VINDINIUM_STATUS_FAILURE,
   VINDINIUM_STATUS_CURL_FAILURE,
   VINDINIUM_STATUS_NULL_POINTER,
-};
+  VINDINIUM_STATUS_BAD_CONFIG,
+  VINDINIUM_STATUS_BUFFER_TOO_SMALL,
+} VindiniumStatus;
 
 typedef struct {
   char const *endpoint;
   char const *key;
-  unsigned int turns;
+  unsigned turns;
   char const *map;
 } VindiniumTrainingConfig;
 
 char const *VINDINIUM_DEFAULT_TRAINING_ENDPOINT = "http://vindinium.org/api/training";
 
 typedef struct {
+  CURL *curl_handle;
   char const *endpoint;
   char const *key;
-  unsigned int current_turn;
-  unsigned int max_turns;
+  unsigned current_turn;
+  unsigned max_turns;
 } VindiniumSession;
 
+#define LOG printf
 
 VindiniumStatus vindinium_cleanup_session(VindiniumSession *aSession) {
   if (aSession == NULL) {
@@ -42,15 +46,70 @@ VindiniumStatus vindinium_create_training_session(
     return VINDINIUM_STATUS_NULL_POINTER;
   }
 
-  CURL *curl_session = curl_easy_init();
-  if (curl_session == NULL) {
+  if (aConfig->key == NULL || strlen(aConfig->key) == 0) {
+    return VINDINIUM_STATUS_BAD_CONFIG;
+  }
+
+  // If an endpoint wasn't specified, use default
+  char const *endpoint = NULL;
+  if (aConfig->endpoint == NULL || strlen(aConfig->endpoint) == 0) {
+    endpoint = VINDINIUM_DEFAULT_TRAINING_ENDPOINT;
+  }
+
+  // Convert the number of turns into a string
+  unsigned const TURNS_STRING_MAX_SIZE = 16;
+  char turns_string[TURNS_STRING_MAX_SIZE] = {0};
+  if (aConfig->turns != 0) {
+    int chars_written = snprintf(turns_string, TURNS_STRING_MAX_SIZE, "%d", aConfig->turns);
+    if (chars_written >= TURNS_STRING_MAX_SIZE) {
+      return VINDINIUM_STATUS_BUFFER_TOO_SMALL;
+    }
+  }
+
+  CURL *curl_handle = curl_easy_init();
+  if (curl_handle == NULL) {
     return VINDINIUM_STATUS_CURL_FAILURE;
   }
 
-  char const *endpoint = NULL;
-  if (aConfig->endpoint == NULL) {
-    endpoint = VINDINIUM_DEFAULT_TRAINING_ENDPOINT;
+  // The unescaped/raw fields we'll POST to the server
+  unsigned const NUM_FIELDS = 3;
+  char const *raw_post_fields[] = {
+    "key", aConfig->key,
+    "turns", turns_string,
+    "map", aConfig->map,
+  };
+
+  // The actual data we'll send to the server
+  unsigned const POST_PAYLOAD_MAX_SIZE = 512;
+  char post_payload[POST_PAYLOAD_MAX_SIZE] = {0};
+
+  // Build the payload by adding each of the post fields (being sure to URL escape each value)
+  unsigned pos = 0;
+  for (unsigned i = 0; i < NUM_FIELDS; ++i) {
+    char const *field_name = raw_post_fields[i];
+    char const *field_value = raw_post_fields[i + 1];
+    if (field_value != NULL && strlen(field_value) > 0) {
+      char *escaped_value = curl_easy_escape(curl_handle, field_value, 0);
+      int chars_written = snprintf(post_payload + pos, POST_PAYLOAD_MAX_SIZE - pos, "%s=%s&",
+                                   field_name, escaped_value);
+      curl_free(escaped_value);
+
+      pos += chars_written;
+      if (pos >= POST_PAYLOAD_MAX_SIZE) {
+        curl_easy_cleanup(curl_handle);
+        return VINDINIUM_STATUS_BUFFER_TOO_SMALL;
+      }
+    }
   }
+
+  {
+    curl_easy_setopt(curl_handle, CURLOPT_URL, endpoint);
+    curl_easy_setopt(curl_handle, CURLOPT_POSTFIELDS, post_payload);
+    CURLcode rc = curl_easy_perform(curl_handle);
+    if (rc != CURLE_OK) {
+      return VINDINIUM_STATUS_CURL_FAILURE;
+    }
+  } 
 
   return VINDINIUM_STATUS_OK;
 }
@@ -62,6 +121,12 @@ int main(void) {
       return 1;
     }
   }
+
+  VindiniumSession *session = NULL;
+  VindiniumTrainingConfig config = {0};
+  config.key = "asdf";
+  VindiniumStatus status = vindinium_create_training_session(&session, &config);
+  printf("\nStatus=%d\n", status);
 
   return 0;
 }
